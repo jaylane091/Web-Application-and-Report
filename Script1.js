f0f5e5420b84b851c293d4c0aa87652ac0563dda
Wabooski9 <jacoblane02@gmail.com>
2020-11-30 21:51:52 +0000

    added colour fade on menu bar and dynamic page chance for smaller devices

--- Script1.js
+++ Script1.js
@@ -1,1 +1,10 @@
 // JavaScript source code
+$(document).ready(function(){
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+        } else {
+            $('.navbar').removeClass("sticky");
+        }
+    })
+});
